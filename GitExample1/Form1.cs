12aa485a67a67fc0a956ee5be977eccdfd26b5e7
DoomDayKross <21110777@student.hcmute.edu.vn>
2024-02-21 08:05:16 +0700

    second

--- GitExample1/Form1.cs
+++ GitExample1/Form1.cs
@@ -1,10 +1,16 @@
-namespace GitExample1
+﻿namespace GitExample1
 {
     public partial class Form1 : Form
     {
+        //Cập nhật lần 1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
