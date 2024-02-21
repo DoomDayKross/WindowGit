8405ea6bee06b90becc46e21e8094e8b592ed3dc
DoomDayKross <21110777@student.hcmute.edu.vn>
2024-02-21 08:26:21 +0700

    cap nhat nhanh con

--- GitExample1/Form1.cs
+++ GitExample1/Form1.cs
@@ -1,7 +1,12 @@
-namespace GitExample1
+﻿namespace GitExample1
 {
     public partial class Form1 : Form
     {
+        //Cập nhật lần 1
+        //Cập nhật nhánh con
+        //Cập nhật nhánh con lần 2
+        //Cập nhật nhánh con lần 3
+        //Cập nhật nhánh con lần 4
         public Form1()
         {
             InitializeComponent();
